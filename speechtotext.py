6e84855d263aad2c30c84d0c3478a4ca1ff35458
MrMiaoMiao <wkmiao@edu.uwaterloo.ca>
2018-05-05 13:26:24 -0400

    added IBM Watson TTS

--- speechtotext.py
+++ speechtotext.py
@@ -1,4 +1,12 @@
 import speech_recognition as sr
+import json
+import os
+from watson_developer_cloud import TextToSpeechV1
+
+tts = TextToSpeechV1(
+    username='c6111125-b9ad-4ecf-83b2-5258b04e5d99',
+    password='ShsWI0ilPixm')
+print(json.dumps(tts.list_voices(), indent=2))
 
 r = sr.Recognizer()
 
@@ -10,20 +18,34 @@ with mic as source:
 
         audio = r.listen(source)
         r.adjust_for_ambient_noise(source, duration=0.5)
+        # Speech recognition using Google Speech Recognition
         try:
+            # for testing purposes, we're just using the default API key
+            # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+            # instead of `r.recognize_google(audio)`
             result = r.recognize_google(audio)
             print("You said " + result)
             words = result.lower()
             if words=="stop":
                 break
+            with open('./output.wav', 'wb') as audio_file:
+                audio_file.write(
+                    tts.synthesize(result, accept='audio/wav', voice="en-US_AllisonVoice").content)
+            print("Allison says")
+            os.system("mpv output.wav")
+            os.system("rm output.wav")
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
         except LookupError:
-            print("Please, speak more clearly")		
+            print("Please, speak more clearly")     
 
 '''
 file = sr.AudioFile('file.wav')
 with file as source:
-	r.adjust_for_ambient_noise(source, duration=0.5)
-	audio = r.record(source)
+    r.adjust_for_ambient_noise(source, duration=0.5)
+    audio = r.record(source)
 '''
 print("done")
 
